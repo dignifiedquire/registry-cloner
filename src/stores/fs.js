14fd3831e28866a3db6750a0fbc06448121f623a
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-01 16:35:06 +0100

    add readFile to the fs store

--- src/stores/fs.js
+++ src/stores/fs.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import {writeFile} from 'fs'
+import {writeFile, readFile} from 'fs'
 
 export default {
   writeFile (p: string, content: string): Promise<void> {
@@ -12,6 +12,19 @@ export default {
         }
 
         resolve()
+      })
+    })
+  },
+
+  readFile (p: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      readFile(p, (err, content) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        resolve(content)
       })
     })
   }
